674bc6cc0260e9c306794442163aedd7cdaf1fc6
Fanta335 <fantayusuke5210@gmail.com>
2021-09-30 16:28:03 +0900

    add command arguments

--- makegif.sh
+++ makegif.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
 input=$1
+fps=$2
+scale=$3
 
 output=$(dirname ${input})/anime.gif
 tmp=$(dirname ${input})/tmp.gif
 echo $output
 
 
-ffmpeg -i $input -vf fps=30,scale=640:-1:flags=lanczos,palettegen palette.png
-ffmpeg -i $input -i palette.png -filter_complex "fps=30,scale=640:-1:flags=lanczos[x];[x][1:v]paletteuse" $tmp
+ffmpeg -i $input -vf fps=${fps},scale=${scale}:-1:flags=lanczos,palettegen palette.png
+ffmpeg -i $input -i palette.png -filter_complex "fps=${fps},scale=${scale}:-1:flags=lanczos[x];[x][1:v]paletteuse" $tmp
 
 gifsicle -O3 --colors=128 --lossy=30 $tmp -o $output
 
